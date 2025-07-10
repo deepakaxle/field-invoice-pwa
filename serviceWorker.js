a9d7fb235d33514ad24b2c81c0db451e3a461d1c
axle forms <deepak@axlefield.com>
2025-07-10 08:34:46 -0600

    serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,20 +1,20 @@
-const CACHE_NAME = 'field-invoice-cache-v3';  // increment to clear old cache
+const CACHE_NAME = 'field-invoice-cache-v3';
 const urlsToCache = [
   './offlineForm.html',
   './dynamicForm.js',
   './style.css',
-  './logo.png',
+  './logo.png', // optional
   './'
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('activate', function (event) {
+self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
@@ -22,7 +22,7 @@ self.addEventListener('activate', function (event) {
   );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request).catch(() =>
       caches.match(event.request).then(response => {
